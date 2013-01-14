c554842c4f4d18601f3b0cc81079060f7f3b596e
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-14 04:34:49 -0500

    Started on a case preserving dictionary

--- cidict.py
+++ cidict.py
@@ -29,3 +29,35 @@ class cidict(dict):
         for key in other:
             # uses our __setitem__, no need to lower() here
             self[key] = other[key]
+
+class cpdict(dict):
+    '''Case insensitive but preserving dictionary.
+    '''
+    
+    def __init__(self, mapping=None, **kwargs):
+        super(cpdict, self).__init__(mapping, **kwargs)
+        
+        self._case_map = {}
+        
+        if mapping:
+            for key in mapping:
+                self._case_map[key.lower()] = key
+        if kwargs:
+            for key in kwargs:
+                self._case_map[key.lower()] = key
+    
+    def __delitem__(self, key):
+        super(cidict, self).__delitem__(key.lower())
+    
+    def __getitem__(self, key):
+        return super(cidict, self).__getitem__(key.lower())
+    
+    def __setitem__(self, key, value):
+        super(cidict, self).__setitem__(key.lower(), value)
+    
+    def __contains__(self, key):
+        return super(cidict, self).__contains__(key.lower())
+    
+    def update(self, other):
+        for key in other:
+            self[key.lower()] = other[key]
