49d6e851aad2281e2708eaf38610ab46c5a31896
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-16 13:01:06 -0500

    More constructor tests

--- tests/cidict_test.py
+++ tests/cidict_test.py
@@ -6,18 +6,43 @@ class CidictTest(unittest.TestCase):
         d = cidict.cidict()
         self.assertEqual(0, len(d))
     
-    def test_init_no_conflict(self):
+    def test_init_maping_no_conflict(self):
+        d = cidict.cidict(dict(foo=1, bar=2))
+        self.assertEqual(2, len(d))
+        self.assertEqual(1, d['foo'])
+        self.assertEqual(2, d['bar'])
+    
+    def test_init_mapping_conflict(self):
+        d = cidict.cidict(dict(foo=1, bar=2, BAR=3))
+        self.assertEqual(2, len(d))
+        self.assertEqual(1, d['foo'])
+        assert d.has_key('bar')
+        assert d['bar'] in [2, 3]
+    
+    def test_init_kwargs_no_conflict(self):
         d = cidict.cidict(foo=1, bar=2)
         self.assertEqual(2, len(d))
         self.assertEqual(1, d['foo'])
         self.assertEqual(2, d['bar'])
     
-    def test_init_conflict(self):
+    def test_init_kwargs_conflict(self):
         d = cidict.cidict(foo=1, bar=2, BAR=3)
         self.assertEqual(2, len(d))
         self.assertEqual(1, d['foo'])
         assert d.has_key('bar')
         assert d['bar'] in [2, 3]
+    
+    def test_init_kwargs_override(self):
+        d = cidict.cidict(dict(foo=1, bar=2), foo=4, bar=5)
+        self.assertEqual(2, len(d))
+        self.assertEqual(4, d['foo'])
+        self.assertEqual(5, d['bar'])
+    
+    def test_init_kwargs_ci_override(self):
+        d = cidict.cidict(dict(foo=1, BAR=2), FOO=4, bar=5)
+        self.assertEqual(2, len(d))
+        self.assertEqual(4, d['foo'])
+        self.assertEqual(5, d['bar'])
     
     def test_case_insensitive_access(self):
         d = cidict.cidict(foo=1, bar=2)
