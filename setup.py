86d379c86b4cf7b3c7c71838d4dc44859c856ffc
Oleg Pudeyev <oleg@bsdpower.com>
2014-02-03 17:07:14 -0500

    Preparing for pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='cidict',
-    version='0.1',
+    version='0.1.1',
     description='Case insensitive dictionary type',
     author='Oleg Pudeyev',
     author_email='oleg@bsdpower.com',
     url='http://github.com/p/cidict',
     py_modules=['cidict'],
+    data_files=['LICENSE', 'README.rst'],
 )
